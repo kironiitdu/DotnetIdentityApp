279d79bac617f930a184d7ef239ac1a5416eb603
Md Farid Uddin <kironiitdu@outlook.com>
2023-06-06 16:40:50 +0800

    updated

---
+++ DotnetIdentityApp/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetIdentityApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public JsonResult TitleAccessUpdate(int? Id)
+        {
+            return Json($"Access Id {Id} has been updated!");
+        }
+    }
+}
